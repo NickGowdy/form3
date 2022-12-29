4abc386c45fe8c193d21b7a167ab0c0199fe4a54
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-12-29 15:20:30 +0000

    simple GET that decodes data successfully

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Hello world...")
+	resp, err := http.Get("http://localhost:8080/v1/organisation/accounts/89faf3cd-fc6e-4e87-b930-00c182cafb05")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var acc AccountResponse
+	if err = json.NewDecoder(resp.Body).Decode(&acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Account is: %v", *acc.AccountData.Attributes.Country)
 }
