6df1e4b3d9ff6ea7e9cdb1ce7a92739c62da6122
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-12-29 16:39:32 +0000

    Playing around with interfaces

--- main.go
+++ main.go
@@ -1,22 +1,51 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
+type API interface {
+	fetch() (http.Response, error)
+	delete() (http.Response, error)
+}
+
+type AccountService struct {
+	id uuid.UUID
+}
+
+func (as AccountService) fetch() (http.Response, error) {
+	url := fmt.Sprintf("http://localhost:8080/v1/organisation/accounts/%s", as.id)
+	fmt.Println(url)
+	resp, err := http.Get(url)
+
+	return *resp, err
+}
+
+// func (a AccountService) fetch() (AccountData, error) {
+// 	url := fmt.Sprintf("http://localhost:8080/v1/organisation/accounts/%s", a.id)
+// 	fmt.Println(url)
+// 	resp, err := http.Get(url)
+// 	var acc AccountResponse
+// 	if err != nil {
+// 		return *acc.AccountData, nil
+// 	}
+
+// 	if err = json.NewDecoder(resp.Body).Decode(&acc); err != nil {
+// 		return *acc.AccountData, nil
+// 	}
+
+// 	return *acc.AccountData, nil
+// }
+
 func main() {
-	resp, err := http.Get("http://localhost:8080/v1/organisation/accounts/89faf3cd-fc6e-4e87-b930-00c182cafb05")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// id, err := uuid.Parse("89faf3cd-fc6e-4e87-b930-00c182cafb05")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	var acc AccountResponse
-	if err = json.NewDecoder(resp.Body).Decode(&acc); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("Account is: %v", *acc.AccountData.Attributes.Country)
+	// as := AccountService{id: id}
+	// as.fetch()
 }
