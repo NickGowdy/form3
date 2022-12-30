949b99223a410f7b8958011823ce4da9d1a5f0df
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-12-30 11:23:18 +0000

    changed test to create record and use it's guid to fetch same record

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -12,17 +12,37 @@ import (
 const endpoint = "organisation/accounts"
 
 type API interface {
-	create() (http.Response, error)
-	fetch(id uuid.UUID) (http.Response, error)
+	create(*AccountCreateRequest) (http.Response, error)
+	fetch(id string) (http.Response, error)
 	delete(id uuid.UUID) (http.Response, error)
 }
 
 type Account struct {
+	AccountResponse AccountResponse
 }
 
 // create implements API
-func (Account) create() (http.Response, error) {
-	panic("unimplemented")
+func (Account) create(ar *AccountCreateRequest) (http.Response, error) {
+	url := fmt.Sprintf("http://localhost:8080/v1/%s", endpoint)
+	b := new(bytes.Buffer)
+	err := json.NewEncoder(b).Encode(&ar)
+	if err != nil {
+		return http.Response{}, err
+	}
+
+	resp, err := http.Post(url, "application/json", b)
+	if err != nil {
+		return http.Response{}, err
+	}
+
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// fmt.Println(body)
+
+	return *resp, err
 }
 
 // delete implements API
@@ -30,7 +50,7 @@ func (Account) delete(id uuid.UUID) (http.Response, error) {
 	panic("unimplemented")
 }
 
-func (as Account) fetch(id uuid.UUID) (http.Response, error) {
+func (as Account) fetch(id string) (http.Response, error) {
 	url := fmt.Sprintf("http://localhost:8080/v1/%s/%s", endpoint, id)
 	fmt.Println(url)
 	resp, err := http.Get(url)
@@ -38,8 +58,25 @@ func (as Account) fetch(id uuid.UUID) (http.Response, error) {
 	return *resp, err
 }
 
-func Get(api API, id uuid.UUID) (AccountData, error) {
+func Get(api API, id string) (AccountData, error) {
 	resp, err := api.fetch(id)
+	var acc AccountResponse
+	if err != nil {
+		return *acc.AccountData, nil
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&acc); err != nil {
+		return *acc.AccountData, nil
+	}
+
+	defer resp.Body.Close()
+
+	return *acc.AccountData, nil
+}
+
+func Create(api API, ar *AccountCreateRequest) (AccountData, error) {
+	resp, err := api.create(ar)
+
 	var acc AccountResponse
 	if err != nil {
 		return *acc.AccountData, nil
@@ -53,11 +90,11 @@ func Get(api API, id uuid.UUID) (AccountData, error) {
 }
 
 func main() {
-	id, err := uuid.Parse("89faf3cd-fc6e-4e87-b930-00c182cafb05")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// id, err := uuid.Parse("89faf3cd-fc6e-4e87-b930-00c182cafb05")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	as := Account{}
-	Get(as, id)
+	// as := Account{}
+	// Get(as, id)
 }
--- main_test.go
+++ main_test.go
@@ -6,10 +6,41 @@ import (
 	"github.com/google/uuid"
 )
 
-func TestHandleGetFooRR(t *testing.T) {
-	// This ID must be created first, can't be hard-coded
-	id := uuid.MustParse("89faf3cd-fc6e-4e87-b930-00c182cafb05")
+func TestCreateFetchDelete(t *testing.T) {
+	id := uuid.NewString()
+	orgId := uuid.NewString()
+	country := "GB"
+	accClassification := "Personal"
+	accAttributes := AccountAttributes{
+		AccountClassification:   &accClassification,
+		AccountNumber:           "10000004",
+		BankID:                  "400302",
+		BankIDCode:              "GBDSC",
+		BaseCurrency:            "GBP",
+		Bic:                     "NWBKGB42",
+		Country:                 &country,
+		Iban:                    "GB28NWBK40030212764204",
+		JointAccount:            new(bool),
+		Name:                    []string{"Nick", "Gowdy"},
+		SecondaryIdentification: id,
+	}
+
+	accRequest := AccountCreateRequest{
+		AccountData: &AccountData{
+			ID:             id,
+			OrganisationID: orgId,
+			Type:           "accounts",
+			Attributes:     &accAttributes,
+		}}
+
 	as := Account{}
+	createdAccData, err := Create(as, &accRequest)
+
+	if err != nil {
+		t.Error("error should not be nil")
+	}
+
+	id = createdAccData.ID
 	accData, err := Get(as, id)
 
 	if err != nil {
