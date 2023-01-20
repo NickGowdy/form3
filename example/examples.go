a7036eadb7d8f850b0e8dcaead407f75bac8d25e
Nick Gowdy <nickgowdy87@gmail.com>
2023-01-20 09:50:38 +0000

    Added example

---
+++ example/examples.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/nickgowdy/form3/account"
+)
+
+func main() {
+
+	godotenv.Load()
+
+	country := "GB"
+	accClassification := "Personal"
+
+	accAttributes := account.AccountAttributes{
+		AccountClassification: &accClassification,
+		AccountNumber:         "10000004",
+		BankID:                "400302",
+		BankIDCode:            "GBDSC",
+		BaseCurrency:          "GBP",
+		Bic:                   "NWBKGB42",
+		Country:               &country,
+		Iban:                  "GB28NWBK40030212764204",
+		JointAccount:          new(bool),
+		Name:                  []string{"Nick", "Gowdy"},
+	}
+
+	acc := account.NewCreateAccount(1, "accounts", &accAttributes, 30)
+	accResponse, err := account.DoCreate(acc)
+
+	if err != nil {
+		log.Fatal("error creating account", err)
+	}
+
+	j, _ := json.Marshal(accResponse)
+	log.Printf("Account %+v", string(j))
+}
